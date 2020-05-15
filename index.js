bf9e831d2f7cbd183f550b70fc4cb4b144afd34f
Grill99 <59368801+Grill99@users.noreply.github.com>
2020-05-15 12:01:25 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const Client = require('./client/Client');
-const { prefix, token } = require('./config.json');
-
-const client = new Client();
-client.commands = new Discord.Collection();
